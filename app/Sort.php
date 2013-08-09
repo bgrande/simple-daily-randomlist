de8e492075aeedde302e2e3275c5a9d2e80315d2
Benedikt Grande <benedikt.grande@mayflower.de>
2013-08-09 10:56:30 +0200

    introduce simple day based list caching

--- app/Sort.php
+++ app/Sort.php
@@ -4,7 +4,6 @@ require_once(APPLICATION_PATH . "/lib/Sortlib.php");
 
 class Sort
 {
-
     /**
      * @var string
      */
@@ -21,11 +20,22 @@ class Sort
     protected $_date;
 
     /**
-     * @param string $sourceFile
+     * bool
      */
-    public function __construct($sourceFile)
+    protected $_useCache;
+
+    /**
+     * string
+     */
+    protected $_cachedOutputFile;
+
+    /**
+     * @param string $sourceFile
+     * @param bool   $useCache
+     */
+    public function __construct($sourceFile, $useCache = true)
     {
-        $this->_sourceFile = APPLICATION_PATH . '/' . $sourceFile;
+        $this->_sourceFile = $sourceFile;
 
         if (file_exists($sourceFile)) {
             $devListJson = file_get_contents($sourceFile);
@@ -33,8 +43,13 @@ class Sort
             $devListJson = '[ "dummy", "dummy2" ]';
         }
 
+        $this->_useCache = $useCache;
         $this->_date = new DateTime();
         $this->_devList = json_decode($devListJson);
+
+        if ($this->_useCache) {
+            $this->_initializeCacheFile();
+        }
     }
 
     /**
@@ -43,9 +58,11 @@ class Sort
     public function getListForCli()
     {
         $sortedList = $this->_sortList();
-        $devs = "today's (" . $this->_date->format('Y-m-d') . ") almost completely random daily list: \n";
+        $title = "today's (" .
+            $this->_date->format('Y-m-d') .
+            ") almost completely random daily list: \n";
 
-        return implode("\n", $sortedList) . "\n";
+        return $title . implode("\n", $sortedList) . "\n";
     }
 
     /**
@@ -58,12 +75,64 @@ class Sort
     }
 
     /**
+     * @return bool|string
+     */
+    protected function _getCachedOutputFileContent()
+    {
+        if (file_exists($this->_cachedOutputFile)) {
+            return file_get_contents($this->_cachedOutputFile);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $fileToDelete
+     */
+    protected function _deleteOutputFile($fileToDelete)
+    {
+        // @todo error handling
+        if (file_exists($fileToDelete)) {
+            unlink($fileToDelete);
+        }
+    }
+
+    /**
      * @return array
      */
     protected function _sortList()
     {
+        $cachedList = $this->_getCachedOutputFileContent();
+        if ($this->_useCache && false != $cachedList) {
+            return json_decode($cachedList);
+        }
+
         $sortlib = new Sortlib($this->_devList);
-        return $sortlib->run($this->_date);
+        $sortedList = $sortlib->run($this->_date);
+
+        if ($this->_useCache) {
+            file_put_contents($this->_cachedOutputFile, json_encode($sortedList));
+        }
+
+        return $sortedList;
     }
 
+    protected function _initializeCacheFile()
+    {
+        $yesterday = new DateTime("-1 day");
+        $basePath = dirname($this->_sourceFile) . '/' .
+            basename($this->_sourceFile, '.json') . '_';
+
+        $fileToDelete = $basePath .
+            $yesterday->format("Y-m-d") .
+            ".json";
+
+        $this->_deleteOutputFile(
+            $fileToDelete
+        );
+
+        $this->_cachedOutputFile = $basePath .
+            $this->_date->format("Y-m-d") .
+            ".json";
+    }
 }
