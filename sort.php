bc3ab3825d174d6c8c40f034a9272f193ce6ca1d
Benedikt Grande <benedikt.grande@mayflower.de>
2013-08-19 16:13:11 +0200

    fixed error handling

--- sort.php
+++ sort.php
@@ -4,22 +4,26 @@ defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FIL
 
 require_once(APPLICATION_PATH . "/app/Sort.php");
 
-$sort = new Sort(APPLICATION_PATH . "/src/devlist.json");
+$error = $sort = null;
 
-if (php_sapi_name() == "cli") {
-    try {
+try {
+    $sort = new Sort(APPLICATION_PATH . "/src/devlist.json");
+} catch (Exception $e) {
+    $error = $e->getMessage();   
+}
+
+if (php_sapi_name() == "cli") { 
+    if (null !== $error) {
+        echo $error . "\n";
+    } else {
         echo $sort->getListForCli();
-    } catch (Exception $e) {
-        echo $e->getMessage() . "\n";
-    }    
-} else {      
-    if ($_GET['resetListAndGenerateNew'] == true) {
-        $sort->resetList();
     }
-
-    try {
+} else {      
+    if (null !== $error) {
+        echo json_encode(array('error' => $error));
+    } else if ($_GET['resetListAndGenerateNew'] == true) {
+        $sort->resetList();
+    } else {
         echo $sort->getListAsJson();
-    } catch (Exception $e) {
-        echo json_encode(array('error' => $e->getMessage()));
-    }    
+    }
 }
