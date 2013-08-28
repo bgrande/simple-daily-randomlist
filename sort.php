45b42601f53fe6ddc0d0dad92cfe53d3e643cdab
Benedikt Grande <benedikt.grande@mayflower.de>
2013-08-28 13:00:18 +0200

    some formatting fixes

--- sort.php
+++ sort.php
@@ -5,7 +5,7 @@ defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FIL
 require_once("bootstrap.php");
 
 $error = $sort = null;
-$type = 'json';
+$type = "json";
 
 if (php_sapi_name() == "cli") { 
     $type = "cli";    
@@ -14,12 +14,12 @@ if (php_sapi_name() == "cli") {
 try {
     $sort = dailySort\App\SortFactory::factory($type, APPLICATION_PATH . "/src/devlist.json");
     
-    if ('json' == $type && $_GET['resetListAndGenerateNew'] == true) {
+    if ("json" == $type && $_GET['resetListAndGenerateNew'] == true) {
         $sort->resetList();
     } 
     
     echo $sort->getSortedList();
 } catch (Exception $e) {
     $error = $e->getMessage();
-    echo ($type == "cli") ? $error . "\n" : json_encode(array('error' => $error));
+    echo ($type == "cli") ? $error . "\n" : json_encode(array("error" => $error));
 }
