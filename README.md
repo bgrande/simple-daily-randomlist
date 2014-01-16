27dae073988709c96f46f1b6dbf27bae7b5415b8
Benedikt Grande <benedikt.grande@mayflower.de>
2014-01-16 09:40:40 +0100

    generalization of filename new multi index mandatorsort cookie and listid handling

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Sorts an array or defined json list for example of names by random
 
 # Requirements
-* a file `src/devlist.json` with json array content (["foo", "bar"] -> see `src/example.json`) must be created
+* a file `src/list.json` with json array content (["foo", "bar"] -> see `src/example.json`) must be created
 
 # Todo
 * multi session with uploaded name list and session id
--- mandatorsort.php
+++ mandatorsort.php
@@ -4,24 +4,38 @@ require_once("bootstrap.php");
 
 $error = $sort = null;
 $type = "json";
+$initiatorSession = null;
+$listId = null;
 
-if (php_sapi_name() == "cli") { 
-    $type = "cli";    
+if ($_COOKIE["initiatorsession"]) {
+    $initiatorSession = $_COOKIE["initiatorsession"];
+} else {
+    $base = time() . '-' . $_SERVER['REMOTE_ADDR'];
+    $hash = hash("sha256", microtime() . '-' . hash("sha256", $base));
+    setcookie("intiatorsession", $hash, time() + 691200);
+    $initiatorSession = $hash;
 }
 
-// @todo get Cookie hash and use it as admin identifier
-// @todo get GET-hash and use it as session identifier
-// @todo add file upload for jsonlist if initial session
+if ($_GET['listid']) {
+    // @todo better use listid as subfolder and save date specific lists in subfolder
+    $listId = trim(preg_replace("([^\w\s\d\-_~,;:\[\]\(\]]|[\.]{2,})", '', $_GET['listid']));
+    $filename = "list-" . $listId . ".json";
+} else {
+    $base = time() . '-' . $_SERVER['REMOTE_ADDR'];
+    $hash = hash("sha256", $base);
+    echo json_encode(array('listid' => $hash));
+    return;
+}
 
 try {
-    $sort = dailySort\App\SortFactory::factory($type, APPLICATION_PATH . "/src/devlist.json");
+    $sort = dailySort\App\SortFactory::factory($type, APPLICATION_PATH . "/src/" . $filename);
     
-    if ("json" == $type && $_GET['resetListAndGenerateNew'] == true) {
+    if ($_GET['resetListAndGenerateNew'] == true) {
         $sort->resetList();
     } 
     
     echo $sort->getSortedList();
 } catch (Exception $e) {
     $error = $e->getMessage();
-    echo ($type == "cli") ? $error . "\n" : json_encode(array("error" => $error));
+    echo json_encode(array("error" => $error));
 }
