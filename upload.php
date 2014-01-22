1ca1c4ee0835d0e53728b614890b558b7f50095d
Benedikt Grande <benedikt.grande@mayflower.de>
2014-01-22 09:25:00 +0100

    basic session checking

--- upload.php
+++ upload.php
@@ -3,28 +3,32 @@
 require_once("bootstrap.php");
 
 $listId = null;
-$initiatorSession = null;
+$isAllowed = false;
 
 try {
+    if (empty($_FILES) || !isset($_FILES['list-upload-file'])) {
+        throw new RuntimeException('No valid file provided!');
+    }
+
     // @todo use own (static) class
     // @todo make session checking right!
-    if (isset($_COOKIE["initiatorsession"])) {
-        $initiatorSession = $_COOKIE["initiatorsession"];
+    // @todo this is not save! we need another credential here
+    if (isset($_GET['listid']) && session_id() == $_GET['listid']) {
+        $hash = trim(preg_replace("([^\w\s\d\-_~,;:\[\]\(\]]|[\.]{2,})", '', $_GET['listid']));
+        $isAllowed = true;
+    } else if (!isset($_GET['listid'])) {
+        $base = time() . '-' . $_SERVER['REMOTE_ADDR'];
+        $hash = hash("sha256", $base);
+        session_id($hash);
+        session_set_cookie_params(time() + 691200);
+        session_start();
     } else {
         throw new RuntimeException('you are not allowed to upload files!');
     }
 
-    // @todo use own (static) class
-    if (isset($_GET['listid'])) {
-        $hash = trim(preg_replace("([^\w\s\d\-_~,;:\[\]\(\]]|[\.]{2,})", '', $_GET['listid']));
-    } else {
-        $base = time() . '-' . $_SERVER['REMOTE_ADDR'];
-        $hash = hash("sha256", $base);
-    }
-
     $targetFilePath = sprintf("src/list-%s.json", $hash);
 
-    if (!empty($_FILES) && isset($_FILES['list-upload-file']) && 'application/octet-stream' == $_FILES['list-upload-file']['type']) {
+    if ('application/octet-stream' == $_FILES['list-upload-file']['type']) {
         $uploadFile = $_FILES['list-upload-file'];
         move_uploaded_file($uploadFile['tmp_name'], $targetFilePath);
     } else {
@@ -32,12 +36,6 @@ try {
     }
 
     $targetUrl = 'mandatorindex.php?listid=' . $hash;
-
-    // @todo make session handling right -> see above
-    session_id($hash);
-    session_name('initiatorsession');
-    session_set_cookie_params(time() + 691200);
-    session_start();
 } catch (Exception $e) {
     $targetUrl = 'mandatorindex.php?error=' . urlencode($e->getMessage());
 }
