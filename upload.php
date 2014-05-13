bda1c7565111066ec9213e1e24796f3f75dc0ee5
Benedikt Grande <benedikt.grande@mayflower.de>
2014-05-13 09:35:50 +0200

    introduced type and email specification for uploaded file

--- upload.php
+++ upload.php
@@ -13,13 +13,18 @@ try {
     $fileHelper = new \sort\Lib\Helper\File();
     $fileHelper->setFile($_FILES['list-upload-file']);
 
+    $email = isset($_GET['email']) ? $_GET['email'] : null;
+    $type = isset($_GET['type']) ? $_GET['type'] : null;
+
     // @todo make session checking right!
     // @todo this is not save! we need another credential here (email with link) (&admin=session_id())
     if (isset($_GET['listid']) && session_id() == $_GET['listid']) {
         $hash = $fileHelper->sanatizeInput($_GET['listid']);
         $isAllowed = true;
     } else if (!isset($_GET['listid'])) {
-        $hash = \sort\Lib\Helper\Hash::createListHash($_SERVER['REMOTE_ADDR']);
+        $salt = $_SERVER['REMOTE_ADDR'] . $email . $type;
+        $hash = \sort\Lib\Helper\Hash::createListHash($salt);
+
         session_id();
         session_set_cookie_params(time() + 691200);
         session_start();
